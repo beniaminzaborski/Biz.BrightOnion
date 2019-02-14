1629990021647c6603fdda55503fb18e081416fa
Beniamin <beniamin.zaborski@gmail.com>
2019-02-14 17:26:03 +0100

    Publish before database migration in Catalog Service

--- Services/Catalog/Biz.BrightOnion.Catalog.API/run_database_migration.bat
+++ Services/Catalog/Biz.BrightOnion.Catalog.API/run_database_migration.bat
@@ -1,4 +1,7 @@
 ﻿rem ### Install global tool: ###
 rem ### dotnet tool install --global FluentMigrator.DotNet.Cli --version 3.1.2 ###
 
+dotnet publish
+cd .\bin\Debug\netcoreapp2.2\publish\
 dotnet fm migrate -p SqlServer2014 -c "Server=.\SQLEXPRESS;Database=BrightOnionCatalog;Trusted_Connection=True;" -a Biz.BrightOnion.Catalog.API.dll
+cd ..\..\..\
