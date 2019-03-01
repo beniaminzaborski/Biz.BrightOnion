24f0999a1f75787392cae545d181a1f213fa77da
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-03-01 10:49:01 +0100

    Move publishing integration events to identity service background tasks project

--- Services/Identity/Biz.BrightOnion.Identity.BackgroundTasks/Startup.cs
+++ Services/Identity/Biz.BrightOnion.Identity.BackgroundTasks/Startup.cs
@@ -1,31 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Biz.BrightOnion.EventBus;
 using Biz.BrightOnion.EventBus.Abstractions;
 using Biz.BrightOnion.EventBus.RabbitMQ;
-using Biz.BrightOnion.Identity.API.Configuration;
-using Biz.BrightOnion.Identity.API.Data;
-using Biz.BrightOnion.Identity.API.Repositories;
-using Biz.BrightOnion.Identity.API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Biz.BrightOnion.Identity.BackgroundTasks.Configuration;
+using Biz.BrightOnion.Identity.BackgroundTasks.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using RabbitMQ.Client;
 
-namespace Biz.BrightOnion.Identity.API
+namespace Biz.BrightOnion.Identity.BackgroundTasks
 {
   public class Startup
   {
@@ -37,27 +28,13 @@ namespace Biz.BrightOnion.Identity.API
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public IServiceProvider ConfigureServices(IServiceCollection services)
     {
-      services.AddHealthChecks()
-        .AddDbContextCheck<ApplicationContext>();
-
-      services.AddDbContext<ApplicationContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-      services.AddScoped<IUserRepository, UserRepository>();
-      services.AddScoped<IIntegrationEventLogRepository, IntegrationEventLogRepository>();
-
-      services.AddScoped<IPasswordHasher, Md5PasswordHasher>();
-      services.AddScoped<IAuthenticationService, JwtAuthenticationService>();
-
-      services.AddScoped<IIntegrationEventLogService, IntegrationEventLogService>();
       services.AddEventBus(Configuration);
 
-      services.AddCors();
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-      services.AddJwtAuthentication(Configuration);
+      // TODO: Add in configuration enabling Background Worker Service !!!
+      services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService, PublishIntegrationEventsService>();
 
       var container = new ContainerBuilder();
       container.Populate(services);
@@ -67,28 +44,6 @@ namespace Biz.BrightOnion.Identity.API
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      app.UseHealthChecks("/hc");
-
-      // global cors policy
-      app.UseCors(x => x
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
-
-      app.UseAuthentication();
-
-      app.UseHttpsRedirection();
-      app.UseMvc();
     }
   }
 
@@ -148,36 +103,6 @@ namespace Biz.BrightOnion.Identity.API
       });
 
       services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
-
-      return services;
-    }
-
-    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
-    {
-      // configure strongly typed settings objects
-      var appSettingsSection = configuration.GetSection("AppSettings");
-      services.Configure<AppSettings>(appSettingsSection);
-
-      // configure jwt authentication
-      var appSettings = appSettingsSection.Get<AppSettings>();
-      var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-      services.AddAuthentication(x =>
-      {
-        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      })
-      .AddJwtBearer(x =>
-      {
-        x.RequireHttpsMetadata = false;
-        x.SaveToken = true;
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = new SymmetricSecurityKey(key),
-          ValidateIssuer = false,
-          ValidateAudience = false
-        };
-      });
 
       return services;
     }
---
+++ Services/Identity/Biz.BrightOnion.Identity.BackgroundTasks/Tasks/PublishIntegrationEventsService.cs
@@ -0,0 +1,138 @@
+﻿using Biz.BrightOnion.EventBus.Abstractions;
+using Biz.BrightOnion.EventBus.Events;
+using Biz.BrightOnion.Identity.BackgroundTasks.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biz.BrightOnion.Identity.BackgroundTasks.Tasks
+{
+  public class PublishIntegrationEventsService : BackgroundService
+  {
+    private readonly IConfiguration configuration;
+    private readonly ILogger<PublishIntegrationEventsService> logger;
+    private readonly IEventBus eventBus;
+    private readonly string connectionString;
+
+    public PublishIntegrationEventsService(
+      IConfiguration configuration,
+      ILogger<PublishIntegrationEventsService> logger,
+      IEventBus eventBus)
+    {
+      this.configuration = configuration;
+      this.logger = logger;
+      this.eventBus = eventBus;
+
+      this.connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+      logger.LogDebug($"PublishIntegrationEventsService is starting.");
+
+      stoppingToken.Register(() =>
+        logger.LogDebug($"PublishIntegrationEventsService background task is stopping."));
+
+      while (!stoppingToken.IsCancellationRequested)
+      {
+        logger.LogDebug($"PublishIntegrationEventsService task doing background work.");
+        
+        await PublishUnpublishedIntegrationEvents();
+
+        await Task.Delay(10000, stoppingToken);
+      }
+
+      logger.LogDebug($"PublishIntegrationEventsService background task is stopping.");
+    }
+
+    private async Task<IDictionary<long, IntegrationEvent>> GetUnpublishedIntegrationEvents()
+    {
+      IDictionary<long, IntegrationEvent> eventsToPublish = new Dictionary<long, IntegrationEvent>();
+
+      using (var connection = new SqlConnection(connectionString))
+      {
+        try
+        {
+          await connection.OpenAsync();
+
+          // TODO: Max Count (default 10) move to settings
+          var sqlQuery = @"
+            SELECT TOP 10 Id, EventType, EventBody FROM dbo.IntegrationEventLogs
+            WHERE State = 0
+            ORDER BY EventCreationDate DESC";
+
+          using (var command = new SqlCommand(sqlQuery, connection))
+          {
+            var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+              long id = reader.GetInt64(0);
+              string eventType = reader.GetString(1);
+              string eventBody = reader.GetString(2);
+
+              var integrationEventType = Type.GetType(eventType);
+              var integrationEvent = JsonConvert.DeserializeObject(eventBody, integrationEventType) as IntegrationEvent;
+
+              eventsToPublish.Add(id, integrationEvent);
+            }
+          }
+        }
+        catch (SqlException exception)
+        {
+          logger.LogCritical(exception, "FATAL ERROR: Database connections could not be opened: {Message}", exception.Message);
+        }
+      }
+
+      return eventsToPublish;
+    }
+
+    private async Task PublishUnpublishedIntegrationEvents()
+    {
+      var eventsToPublish = await GetUnpublishedIntegrationEvents();
+
+      if (eventsToPublish.Count == 0)
+        return;
+
+      using (var connection = new SqlConnection(connectionString))
+      {
+        try
+        {
+          await connection.OpenAsync();
+
+          foreach (var eventToPublish in eventsToPublish)
+          {
+            eventBus.Publish(eventToPublish.Value);
+            await MarkEventAsPublishedAsync(connection, eventToPublish.Key);
+          }
+        }
+        catch (Exception exception)
+        {
+          logger.LogCritical(exception, "FATAL ERROR: Database connections could not be opened: {Message}", exception.Message);
+        }
+      }
+    }
+
+    private async Task MarkEventAsPublishedAsync(SqlConnection connection, long eventLogId)
+    {
+      string sqlQuery = @"UPDATE dbo.IntegrationEventLogs SET State = 1 WHERE Id = @id";
+      using (var command = new SqlCommand(sqlQuery, connection))
+      {
+        command.Parameters.AddWithValue("@id", eventLogId);
+        await command.ExecuteNonQueryAsync();
+      }
+    }
+
+    //public override async Task StopAsync(CancellationToken stoppingToken)
+    //{
+    //  // Run your clean-up actions
+    //}
+  }
+}
