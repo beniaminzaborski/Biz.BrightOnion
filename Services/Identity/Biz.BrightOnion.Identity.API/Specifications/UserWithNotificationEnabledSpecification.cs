21a6a457e88846521e221faf251f38c74b08fbb6
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-03-06 11:24:05 +0100

    Identity Service: add an unused sample specification

---
+++ Services/Identity/Biz.BrightOnion.Identity.API/Specifications/UserWithNotificationEnabledSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Biz.BrightOnion.Identity.API.Entities;
+using Biz.BrightOnion.Identity.API.Infrastructure.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biz.BrightOnion.Identity.API.Specifications
+{
+  public class UserWithNotificationEnabledSpecification : BaseSpecification<User>
+  {
+    public UserWithNotificationEnabledSpecification(int basketId)
+    : base(u => u.NotificationEnabled == true) { }
+  }
+}
