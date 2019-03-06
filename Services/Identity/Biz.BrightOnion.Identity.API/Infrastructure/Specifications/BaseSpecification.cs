3bad93efe9aac7d0d74f9eb11757590931def202
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-03-06 11:23:15 +0100

    Identity Service: add generic constraint for Entity in base specification

--- Services/Identity/Biz.BrightOnion.Identity.API/Infrastructure/Specifications/BaseSpecification.cs
+++ Services/Identity/Biz.BrightOnion.Identity.API/Infrastructure/Specifications/BaseSpecification.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿using Biz.BrightOnion.Identity.API.Infrastructure.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Biz.BrightOnion.Identity.API.Infrastructure.Specifications
 {
-  public abstract class BaseSpecification<T> : ISpecification<T>
+  public abstract class BaseSpecification<TEntity> : ISpecification<TEntity>
+    where TEntity: Entity
   {
-    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    public BaseSpecification(Expression<Func<TEntity, bool>> criteria)
     {
       Criteria = criteria;
     }
-    public Expression<Func<T, bool>> Criteria { get; }
-    public List<Expression<Func<T, object>>> Includes { get; } =
-    new List<Expression<Func<T, object>>>();
+    public Expression<Func<TEntity, bool>> Criteria { get; }
+    public List<Expression<Func<TEntity, object>>> Includes { get; } =
+    new List<Expression<Func<TEntity, object>>>();
     public List<string> IncludeStrings { get; } = new List<string>();
-    protected virtual void AddInclude(Expression<Func<T, object>> includeExpression)
+    protected virtual void AddInclude(Expression<Func<TEntity, object>> includeExpression)
     {
       Includes.Add(includeExpression);
     }
