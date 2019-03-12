53e38b80459339f9cb20ac2c9ad2818012298a4e
Beniamin <beniamin.zaborski@gmail.com>
2019-03-12 22:22:55 +0100

    Ordering Service: Add unique indexes for Orders and OrderItems tables

---
+++ Services/Ordering/Biz.BrightOnion.Ordering.Infrastructure/Migrations/20190312221101_AlterTable_OrderItemss_Indexes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biz.BrightOnion.Ordering.Infrastructure.Migrations
+{
+  [DbContext(typeof(OrderingContext))]
+  [Migration("20190312221101_AlterTable_OrderItems_Indexes")]
+  public partial class _20190312221101_AlterTable_OrderItems_Indexes : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AddUniqueConstraint(
+        "UQ__OrderItems_OrderId_PurchaserId", 
+        "OrderItems",
+        new string[] { "OrderId", "PurchaserId" });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder) { }
+  }
+}
---
+++ Services/Ordering/Biz.BrightOnion.Ordering.Infrastructure/Migrations/20190312221901_AlterTable_Orders_Indexes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biz.BrightOnion.Ordering.Infrastructure.Migrations
+{
+  [DbContext(typeof(OrderingContext))]
+  [Migration("20190312221901_AlterTable_Orders_Indexes")]
+  public partial class _20190312221901_AlterTable_Orders_Indexes : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AddUniqueConstraint(
+        "UQ__Orders_RoomId_Day",
+        "Orders",
+        new string[] { "RoomId", "Day" });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder) { }
+  }
+}
