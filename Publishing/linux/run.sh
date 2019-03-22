87260480ad0e7a7accce41b10d1f328357b58728
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-03-22 08:57:27 +0100

    Linux shell script to run app

---
+++ Publishing/linux/run.sh
@@ -0,0 +1,45 @@
+cd Web
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_URLS=http://+:5000
+export ASPNETCORE_HTTP_PORT=5000
+dotnet Biz.BrightOnion.Web.dll &
+cd .. 
+
+cd ApiGateway
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_URLS=http://+:7000
+export ASPNETCORE_HTTP_PORT=7000
+dotnet Biz.BrightOnion.ApiGateway.dll &
+cd ..
+
+cd Services/Identity.API
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_URLS=http://+:7001
+export ASPNETCORE_HTTP_PORT=7001
+dotnet Biz.BrightOnion.Identity.API.dll &
+cd ../..
+
+#cd Services/Identity.BackgroundTasks
+#export ASPNETCORE_ENVIRONMENT=Development
+#dotnet Biz.BrightOnion.Identity.BackgroundTasks.dll & 
+#cd ../..
+
+cd Services/Catalog.API
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_URLS=http://+:7002
+export ASPNETCORE_HTTP_PORT=7002
+dotnet Biz.BrightOnion.Catalog.API.dll &
+cd ../..
+
+#cd Services/Catalog.BackgroundTasks
+#export ASPNETCORE_ENVIRONMENT=Development 
+#dotnet Biz.BrightOnion.Catalog.BackgroundTasks.dll &
+#cd ../..
+
+cd Services/Ordering.API
+export ASPNETCORE_ENVIRONMENT=Development 
+export ASPNETCORE_URLS=http://+:7003
+export ASPNETCORE_HTTP_PORT=7003
+dotnet Biz.BrightOnion.Ordering.API.dll &
+cd ../..
+
