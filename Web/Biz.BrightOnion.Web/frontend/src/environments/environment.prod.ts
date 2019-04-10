570170759d007b0271a17b2cabaf6bf88c3d0ccd
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-04-10 13:57:41 +0200

    Frontend app: REST urls for production environment

--- Web/Biz.BrightOnion.Web/frontend/src/environments/environment.prod.ts
+++ Web/Biz.BrightOnion.Web/frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  accountServiceUrl: "http://10.1.0.178:9000/identity-api/account",
+  roomServiceUrl: "http://10.1.0.178:9000/catalog-api/room",
+  orderServiceUrl: "http://10.1.0.178:9000/ordering-api/v1/orders",
+  orderSignalrHubUrl: "http://10.1.0.178:9000/notificationhub"
 };
