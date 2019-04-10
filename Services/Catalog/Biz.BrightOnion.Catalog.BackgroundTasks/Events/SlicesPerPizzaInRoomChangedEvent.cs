49a16f095774fcea9938798a7fc31c91d9c92fa2
Beniamin Zaborski <beniamin.zaborski@infover.pl>
2019-04-10 13:56:39 +0200

    Catalog Background Tasks: Add missing integration events

---
+++ Services/Catalog/Biz.BrightOnion.Catalog.BackgroundTasks/Events/SlicesPerPizzaInRoomChangedEvent.cs
@@ -0,0 +1,24 @@
+﻿using Biz.BrightOnion.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biz.BrightOnion.Catalog.API.Events
+{
+  public class SlicesPerPizzaInRoomChangedEvent : IntegrationEvent
+  {
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int SlicesPerPizza { get; set; }
+
+    public SlicesPerPizzaInRoomChangedEvent(long id, string name, int slicesPerPizza)
+    {
+      Id = id;
+      Name = name;
+      SlicesPerPizza = slicesPerPizza;
+    }
+  }
+}
