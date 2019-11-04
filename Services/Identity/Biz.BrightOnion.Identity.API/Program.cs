edb9d5ca5280aaa55a84d0008fe74107e252a52f
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2019-11-04 07:44:56 +0100

    Migration Identity service to .NET Core 3

--- Services/Identity/Biz.BrightOnion.Identity.API/Program.cs
+++ Services/Identity/Biz.BrightOnion.Identity.API/Program.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Biz.BrightOnion.Identity.API
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateWebHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
-  }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        // Set properties and call methods on options
+                    })
+                    .UseStartup<Startup>();
+                });
+    }
 }
--- Services/Identity/Biz.BrightOnion.Identity.API/Startup.cs
+++ Services/Identity/Biz.BrightOnion.Identity.API/Startup.cs
@@ -16,165 +16,172 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using RabbitMQ.Client;
 
 namespace Biz.BrightOnion.Identity.API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public IServiceProvider ConfigureServices(IServiceCollection services)
-    {
-      services.AddHealthChecks()
-        .AddDbContextCheck<ApplicationContext>();
-
-      services.AddDbContext<ApplicationContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-      services.AddScoped<IUserRepository, UserRepository>();
-      services.AddScoped<IIntegrationEventLogRepository, IntegrationEventLogRepository>();
-
-      services.AddScoped<IPasswordHasher, Md5PasswordHasher>();
-      services.AddScoped<IAuthenticationService, JwtAuthenticationService>();
-
-      services.AddScoped<IIntegrationEventLogService, IntegrationEventLogService>();
-      services.AddEventBus(Configuration);
-
-      services.AddCors();
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-      services.AddJwtAuthentication(Configuration);
-
-      var container = new ContainerBuilder();
-      container.Populate(services);
-      return new AutofacServiceProvider(container.Build());
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      app.UseHealthChecks("/hc");
-
-      // global cors policy
-      app.UseCors(x => x
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
-
-      app.UseAuthentication();
-
-      app.UseHttpsRedirection();
-      app.UseMvc();
-    }
-  }
-
-  public static class CustomExtensionMethods
-  {
-    public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration)
-    {
-      var appSettingsSection = configuration.GetSection("AppSettings");
-      var appSettings = appSettingsSection.Get<AppSettings>();
-
-      // RabbitMQ Persistent Connection
-      services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
-      {
-        var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
-
-        var factory = new ConnectionFactory()
+        public Startup(IConfiguration configuration)
         {
-          HostName = appSettings.EventBusConnection
-        };
-
-        if (!string.IsNullOrEmpty(appSettings.EventBusUserName))
-        {
-          factory.UserName = appSettings.EventBusUserName;
+            Configuration = configuration;
         }
 
-        if (!string.IsNullOrEmpty(appSettings.EventBusPassword))
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-          factory.Password = appSettings.EventBusPassword;
+            services.AddHealthChecks()
+              .AddDbContextCheck<ApplicationContext>();
+
+            services.AddDbContext<ApplicationContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IIntegrationEventLogRepository, IntegrationEventLogRepository>();
+
+            services.AddScoped<IPasswordHasher, Md5PasswordHasher>();
+            services.AddScoped<IAuthenticationService, JwtAuthenticationService>();
+
+            services.AddScoped<IIntegrationEventLogService, IntegrationEventLogService>();
+            services.AddEventBus(Configuration);
+
+            services.AddCors();
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddJwtAuthentication(Configuration);
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+            return new AutofacServiceProvider(container.Build());
         }
 
-        var retryCount = 5;
-        if (appSettings.EventBusRetryCount.HasValue)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          retryCount = appSettings.EventBusRetryCount.Value;
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHealthChecks("/hc");
+
+            // global cors policy
+            app.UseCors(x => x
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+
+            // app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
-
-        return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
-      });
-
-      var subscriptionClientName = appSettings.SubscriptionClientName;
-
-      // RabbitMQ Event Bus
-      services.AddSingleton<IEventBus, RabbitMqEventBus>(sp =>
-      {
-        var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
-        var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
-        var logger = sp.GetRequiredService<ILogger<RabbitMqEventBus>>();
-        var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
-
-        var retryCount = 5;
-        if (appSettings.EventBusRetryCount.HasValue)
-        {
-          retryCount = appSettings.EventBusRetryCount.Value;
-        }
-
-        return new RabbitMqEventBus(rabbitMQPersistentConnection, logger, iLifetimeScope, eventBusSubcriptionsManager, subscriptionClientName, retryCount);
-      });
-
-      services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
-
-      return services;
     }
 
-    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    public static class CustomExtensionMethods
     {
-      // configure strongly typed settings objects
-      var appSettingsSection = configuration.GetSection("AppSettings");
-      services.Configure<AppSettings>(appSettingsSection);
-
-      // configure jwt authentication
-      var appSettings = appSettingsSection.Get<AppSettings>();
-      var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-      services.AddAuthentication(x =>
-      {
-        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      })
-      .AddJwtBearer(x =>
-      {
-        x.RequireHttpsMetadata = false;
-        x.SaveToken = true;
-        x.TokenValidationParameters = new TokenValidationParameters
+        public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration)
         {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = new SymmetricSecurityKey(key),
-          ValidateIssuer = false,
-          ValidateAudience = false
-        };
-      });
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            var appSettings = appSettingsSection.Get<AppSettings>();
 
-      return services;
+            // RabbitMQ Persistent Connection
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = appSettings.EventBusConnection
+                };
+
+                if (!string.IsNullOrEmpty(appSettings.EventBusUserName))
+                {
+                    factory.UserName = appSettings.EventBusUserName;
+                }
+
+                if (!string.IsNullOrEmpty(appSettings.EventBusPassword))
+                {
+                    factory.Password = appSettings.EventBusPassword;
+                }
+
+                var retryCount = 5;
+                if (appSettings.EventBusRetryCount.HasValue)
+                {
+                    retryCount = appSettings.EventBusRetryCount.Value;
+                }
+
+                return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
+            });
+
+            var subscriptionClientName = appSettings.SubscriptionClientName;
+
+            // RabbitMQ Event Bus
+            services.AddSingleton<IEventBus, RabbitMqEventBus>(sp =>
+            {
+                var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                var logger = sp.GetRequiredService<ILogger<RabbitMqEventBus>>();
+                var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+                var retryCount = 5;
+                if (appSettings.EventBusRetryCount.HasValue)
+                {
+                    retryCount = appSettings.EventBusRetryCount.Value;
+                }
+
+                return new RabbitMqEventBus(rabbitMQPersistentConnection, logger, iLifetimeScope, eventBusSubcriptionsManager, subscriptionClientName, retryCount);
+            });
+
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            // configure strongly typed settings objects
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
     }
-  }
 }
