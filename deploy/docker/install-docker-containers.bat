6fec33345c17abc4eb770177d73745185ad48a17
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2019-11-16 11:13:05 +0100

    Temporary comment installing sql & rabbit docker containers in deploy script

--- deploy/docker/install-docker-containers.bat
+++ deploy/docker/install-docker-containers.bat
@@ -1,10 +1,10 @@
-docker network create biz_onion_net
+rem docker network create biz_onion_net
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=H0t0ni0n!" -p 14330:1433 --net biz_onion_net --name biz_onion_sql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04 
-docker run -d -p 15672:15672 -p 5672:5672 --net biz_onion_net --name biz_onion_rabbit rabbitmq:3-management
+rem docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=H0t0ni0n!" -p 14330:1433 --net biz_onion_net --name biz_onion_sql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04 
+rem docker run -d -p 15672:15672 -p 5672:5672 --net biz_onion_net --name biz_onion_rabbit rabbitmq:3-management
 
-docker container run -p 7001:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_identity_api_1 biz_onion_identity_api
-docker container run -p 7011:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_identity_bgt_1 biz_onion_identity_bgt
+docker container run -p 7001:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_identity_api_1 -d biz_onion_identity_api
+docker container run -p 7011:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_identity_bgt_1 -d biz_onion_identity_bgt
 
-docker container run -p 7002:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_catalog_api_1 biz_onion_catalog_api
-docker container run -p 7012:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_catalog_bgt_1 biz_onion_catalog_bgt
+docker container run -p 7002:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_catalog_api_1 -d biz_onion_catalog_api
+docker container run -p 7012:80 --net biz_onion_net -e ASPNETCORE_ENVIRONMENT="Docker" --name biz_onion_catalog_bgt_1 -d biz_onion_catalog_bgt
