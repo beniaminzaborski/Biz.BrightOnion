b53dc81b1c3e9c811b550d1ecc2daba200989f6c
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2020-01-13 17:08:32 +0100

    Extends docker-compose

--- src/Web/Biz.BrightOnion.Web/frontend/src/environments/environment.docker.ts
+++ src/Web/Biz.BrightOnion.Web/frontend/src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: true,
-    accountServiceUrl: "http://biz_onion_gw_1/identity-api/account",
-    roomServiceUrl: "http://biz_onion_gw_1/catalog-api/room",
-    orderServiceUrl: "http://biz_onion_gw_1/ordering-api/v1/orders",
-    orderSignalrHubUrl: "http://biz_onion_gw_1/notificationhub"
+  production: true,
+  accountServiceUrl: "http://localhost:9000/identity-api/account",
+  roomServiceUrl: "http://localhost:9000/catalog-api/room",
+  orderServiceUrl: "http://localhost:9000/ordering-api/v1/orders",
+  orderSignalrHubUrl: "http://localhost:9000/notificationhub"
 };
