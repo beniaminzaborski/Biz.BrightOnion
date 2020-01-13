e91a9d920445f9b5700ddfaea27676564bc7dd9d
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2020-01-13 17:15:10 +0100

    Remove unused environments and old deploying scripts

--- deploy/build/build-production.ps1
+++
@@ -1,5 +0,0 @@
-cd ..\..\Web\Biz.BrightOnion.Web\frontend
-npm install
-npm run-script build -- --environment=prod
-cd ..\..\..
-dotnet publish .\Biz.BrightOnion.sln
--- deploy/publish/windows/stop.ps1
+++
@@ -1,2 +0,0 @@
-
-Stop-Process -Name "Biz.BrightOnion.*"
