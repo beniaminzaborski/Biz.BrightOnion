67ca17206f35eda2a0b165b8b7a4cd41be6437fb
Beniamin Zaborski <beniamin.zaborski@beqom.com>
2020-01-29 19:03:29 +0100

    Add service discovery with fake load balancer to API ordering aggregator gateway

---
+++ src/Infrastructure/Biz.BrightOnion.ServicesRegistry.Consul/LoadBalancers/FakeGetFirstServiceLoadBalancer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biz.BrightOnion.ServicesRegistry.Consul.LoadBalancers
+{
+    public class FakeGetFirstServiceLoadBalancer : IServiceLoadBalancer
+    {
+        public ServiceInfo GetService(IEnumerable<ServiceInfo> services)
+        {
+            return services.First();
+        }
+    }
+}
